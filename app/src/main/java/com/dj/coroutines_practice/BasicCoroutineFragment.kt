27ea449ce311276934047005ce27bfe06d5737ab
DJDrama <putzzang@naver.com>
2021-09-23 23:39:16 +0900

    Job Cancel

--- app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
+++ app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
     private val TAG = this::class.simpleName
     private val coroutineScopeMainImmediate = CoroutineScope(Dispatchers.Main.immediate)
+
+    /** Job 타입 변수 추가 */
+    private var job: Job? = null
 
     private lateinit var button: Button
     private lateinit var tvTime: TextView
@@ -28,10 +33,13 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
 
         button = view.findViewById(R.id.button)
         button.setOnClickListener {
-            coroutineScopeMainImmediate.launch {
+            /** launch는 해당 coroutine의 job을 리턴하므로 job 변수에 assign 해준다 */
+            job = coroutineScopeMainImmediate.launch {
                 tvMessage.text = "Started"
                 button.isEnabled = false
                 runTime()
+                /** 끝나면 Toast 를 띄우기! */
+                Toast.makeText(requireContext(), "Ended!", Toast.LENGTH_LONG).show()
                 tvMessage.text = "Ended"
                 button.isEnabled = true
             }
@@ -44,5 +52,11 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
             tvTime.text = "${seconds--}"
             delay(1000)
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        /** 홈화면으로 화면전환이 이루어질 시 해당 job을 취소한다 */
+        job?.cancel()
     }
 }
