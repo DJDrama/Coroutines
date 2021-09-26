7023906b6e145f9ee628a7927456f471c7519543
DJDrama <putzzang@naver.com>
2021-09-27 00:19:17 +0900

    Cancellation Exception Handling

--- app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
+++ app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
@@ -1,16 +1,13 @@
 package com.dj.coroutines_practice
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
     private val TAG = this::class.simpleName
@@ -33,15 +30,22 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
 
         button = view.findViewById(R.id.button)
         button.setOnClickListener {
-            /** launch는 해당 coroutine의 job을 리턴하므로 job 변수에 assign 해준다 */
             job = coroutineScopeMainImmediate.launch {
-                tvMessage.text = "Started"
-                button.isEnabled = false
-                runTime()
-                /** 끝나면 Toast 를 띄우기! */
-                Toast.makeText(requireContext(), "Ended!", Toast.LENGTH_LONG).show()
-                tvMessage.text = "Ended"
-                button.isEnabled = true
+                try {
+                    tvMessage.text = "Started"
+                    button.isEnabled = false
+                    runTime()
+                    /** 끝나면 Toast 를 띄우기! */
+                    Toast.makeText(requireContext(), "Ended!", Toast.LENGTH_LONG).show()
+                    tvMessage.text = "Ended"
+                    button.isEnabled = true
+                } catch (e: CancellationException) {
+                    e.printStackTrace()
+                    tvMessage.text = "Cancelled"
+                } finally {
+                    button.isEnabled = true
+                    tvTime.text = ""
+                }
             }
         }
     }
@@ -49,6 +53,7 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
     private suspend fun runTime() {
         var seconds = 5
         while (seconds >= 0) {
+            Log.d(TAG, "Remaining Seconds: $seconds")
             tvTime.text = "${seconds--}"
             delay(1000)
         }
@@ -56,7 +61,6 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
 
     override fun onStop() {
         super.onStop()
-        /** 홈화면으로 화면전환이 이루어질 시 해당 job을 취소한다 */
         job?.cancel()
     }
 }
