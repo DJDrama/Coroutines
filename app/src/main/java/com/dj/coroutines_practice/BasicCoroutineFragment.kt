e8ede76fff4ed33d86ef73009a465cc8321ae350
DJDrama <putzzang@naver.com>
2021-09-29 23:03:31 +0900

    Update Kotlin Version

--- app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
+++ app/src/main/java/com/dj/coroutines_practice/BasicCoroutineFragment.kt
@@ -7,7 +7,11 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
     private val TAG = this::class.simpleName
@@ -30,8 +34,10 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
 
         button = view.findViewById(R.id.button)
         button.setOnClickListener {
+            /** launch는 해당 coroutine의 job을 리턴하므로 job 변수에 assign 해준다 */
             job = coroutineScopeMainImmediate.launch {
-                try {
+                var i=0
+                while(i<5) {
                     tvMessage.text = "Started"
                     button.isEnabled = false
                     runTime()
@@ -39,12 +45,6 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
                     Toast.makeText(requireContext(), "Ended!", Toast.LENGTH_LONG).show()
                     tvMessage.text = "Ended"
                     button.isEnabled = true
-                } catch (e: CancellationException) {
-                    e.printStackTrace()
-                    tvMessage.text = "Cancelled"
-                } finally {
-                    button.isEnabled = true
-                    tvTime.text = ""
                 }
             }
         }
@@ -53,7 +53,7 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
     private suspend fun runTime() {
         var seconds = 5
         while (seconds >= 0) {
-            Log.d(TAG, "Remaining Seconds: $seconds")
+            Log.e(TAG, "Remaining Time: $seconds")
             tvTime.text = "${seconds--}"
             delay(1000)
         }
@@ -61,6 +61,7 @@ class BasicCoroutineFragment : Fragment(R.layout.fragment_coroutine_basic) {
 
     override fun onStop() {
         super.onStop()
+        /** 홈화면으로 화면전환이 이루어질 시 해당 job을 취소한다 */
         job?.cancel()
     }
 }
